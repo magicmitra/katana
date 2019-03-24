0b80b86c712df03594fec585efbf189ac2426ead
erv <ervinmitra@gmail.com>
2019-03-24 01:05:48 -0700

    mongodb bitches

---
+++ src/MongoSensei.js
@@ -0,0 +1,29 @@
+/* Replicate lowdb data using mongodb
+ * just cuz we can
+ * dbDevTest -> name if the DB to hold collections of businesses
+ */
+
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/';
+const businessLAFake = require('../tests/businessLAFake');
+
+MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    // create DB object
+    const dbo = db.db('dbDevTest');
+    // create a collection
+    dbo.createCollection('businesses', (err, res) => {
+        if(err) throw err;
+        console.log('Collection Created');
+    });
+    // insert a document into a collection, collections.insert is 
+    // deprecated, therefore, use insertMany.
+    dbo.collection('businesses').insertMany(businessLAFake, (err, res) => {
+        if(err) throw err;
+        console.log("documents inserted");
+        // res.ops for the document section only
+        //console.log(res.ops); 
+        db.close();
+    });
+  });
