c4e5a93a4c84a039584c9b25c36fe2218b37d4b4
erv <ervinmitra@gmail.com>
2019-03-25 11:38:59 -0700

    ran tests

--- src/MongoSensei.js
+++ src/MongoSensei.js
@@ -8,27 +8,47 @@ require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const MongoClient = require('mongodb').MongoClient;
 const Server = require('mongodb').Server;
 const dbLocalHost = process.env.dbLocalHost;
-const url = `mongodb://localhost:${dbLocalHost}/`;
 const businessLAFake = require('../tests/businessLAFake');
 
 
-const mongoClient = new MongoClient(new Server('localhost', `${dbLocalHost}`));
-mongoClient.connect((err, mongoClient) => {
-    if (err) throw err;
-    // create DB object
-    const dbo = mongoClient.db('dbDevTest');
-    // create a collection
-    dbo.createCollection('businesses', (err, res) => {
-        if(err) throw err;
-        console.log('Collection Created');
-    });
-    // insert a document into a collection, collections.insert is 
-    // deprecated, therefore, use insertMany.
-    dbo.collection('businesses').insertMany(businessLAFake, (err, res) => {
-        if(err) throw err;
-        console.log("documents inserted");
-        // res.ops for the document section only
-        //console.log(res.ops); 
-        mongoClient.close();
-    });
-});
+class MongoSensei {
+    //==============================constructor=========================================
+    /** 
+     * @param {*} nameOfDatabase -> name of the database
+     * initialize the Database, MongoClient and Database Object
+     */
+    constructor(nameOfDatabase) {
+        this.nameOfDatabase = nameOfDatabase;
+        this.mongoClient = new MongoClient(new Server('localhost', `${dbLocalHost}`));
+        this.collectionName = 'businesses';
+    }
+    //====================================================================================
+
+    //===============================seed the database====================================
+    seed(items) {
+        try {
+            // connect MongoClient
+            this.mongoClient.connect((err, mongoClient) => {
+                if(err) throw err;
+                const dbObject = this.mongoClient.db(this.nameOfDatabase);
+                // create a collection
+                dbObject.createCollection(this.collectionName, (err, res) => {
+                    if(err) throw err;
+                });
+                // seed the created collection. use insertMany()
+                dbObject.collection(this.collectionName).insertMany(items, (err, res) => {
+                    if(err) throw err;
+                    mongoClient.close();
+                });
+            });
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+    //=====================================================================================
+}
+
+module.exports = {
+    MongoSensei,
+};
