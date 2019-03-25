91b863775dc716dacf6438358b6c1a8b643bba5f
erv <ervinmitra@gmail.com>
2019-03-25 09:32:18 -0700

    restructured mongo sensei

--- src/MongoSensei.js
+++ src/MongoSensei.js
@@ -6,14 +6,17 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const MongoClient = require('mongodb').MongoClient;
+const Server = require('mongodb').Server;
 const dbLocalHost = process.env.dbLocalHost;
 const url = `mongodb://localhost:${dbLocalHost}/`;
 const businessLAFake = require('../tests/businessLAFake');
 
-MongoClient.connect(url, (err, db) => {
+
+const mongoClient = new MongoClient(new Server('localhost', `${dbLocalHost}`));
+mongoClient.connect((err, mongoClient) => {
     if (err) throw err;
     // create DB object
-    const dbo = db.db('dbDevTest');
+    const dbo = mongoClient.db('dbDevTest');
     // create a collection
     dbo.createCollection('businesses', (err, res) => {
         if(err) throw err;
@@ -26,6 +29,6 @@ MongoClient.connect(url, (err, db) => {
         console.log("documents inserted");
         // res.ops for the document section only
         //console.log(res.ops); 
-        db.close();
+        mongoClient.close();
     });
-  });
+});
